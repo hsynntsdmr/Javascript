74ec8fe4af24a9d4613fe9131d0a9ea453e78995
hsynntsdmr <hsynntsdmr@hotmail.com>
2023-09-28 22:42:22 +0300

    loop

---
+++ Loop/main.js
@@ -0,0 +1,8 @@
+const countries = ["Almanya", "Hollanda", "Fransa", "Türkiye", "İngiltere"];
+new arry = [];
+
+for(i = 0; i <= countries.length; i++){
+arry.push(countries[i].toUpperCase());
+
+}
+console.log(arry);
